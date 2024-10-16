3e30e84acb01e3ec148566d837cce4ce88abde53
Amalie <amalie.andreasen99@hotmail.com>
2024-10-16 09:27:05 +0200

    sdsdd

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -1,6 +1,5 @@
 const readyButton = document.querySelector(".ready");
 const landingPage = document.querySelector(".fadeOut");
-const swup = new Swup();
 
 readyButton.addEventListener('click', function(){
   landingPage.classList.add('fadeOutActive'); 
