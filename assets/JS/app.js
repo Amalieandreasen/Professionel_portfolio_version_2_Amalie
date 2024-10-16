66361032f4d38353384eafb5724c258141a5c56a
Amalie <amalie.andreasen99@hotmail.com>
2024-10-16 07:39:25 +0200

    begyndt på dwine

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -1,5 +1,6 @@
 const readyButton = document.querySelector(".ready");
 const landingPage = document.querySelector(".fadeOut");
+const swup = new Swup();
 
 readyButton.addEventListener('click', function(){
   landingPage.classList.add('fadeOutActive'); 
